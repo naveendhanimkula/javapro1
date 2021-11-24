49882a3a2376357dbacf40c60391a3946da667f5
ndhanemkula <ndhanemkula@host.docker.internal>
2021-11-23 20:34:52 -0800

    frist commit

---
+++ naveen/src/naveen/raj.java
@@ -0,0 +1,14 @@
+package naveen;
+
+public class raj {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=13;
+		int c=a+b;
+		System.out.println("sum of two numbers ="+c);
+        System.out.println("sucess");
+	}
+
+}
